22c103ecd49e526d2e0201e8573a693765894ec3
Danielly Egito <egitodany@gmail.com>
2022-06-01 18:00:59 -0300

    fix(Home): ajusta src das imagens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,25 @@
 import { Container } from "./style";
 import Primary from "../../components/PrimaryButton";
-import Secundary from "../../components/SecundaryButton"
+import Secundary from "../../components/SecundaryButton";
+import Logo from "../../assets/logo.png";
+import Planet from "../../assets/planet-icon.png";
 
 function Home() {
   return (
     <Container>
       <header>
         <p>Bem vindo ao</p>
-        <img src=""/>
+        <img src={Logo} />
       </header>
 
-      <img />
+      <img src={Planet} />
 
       <div>
         <Primary>Login</Primary>
         <Secundary>Cadastro</Secundary>
       </div>
     </Container>
-    );
+  );
 }
 
 export default Home;
