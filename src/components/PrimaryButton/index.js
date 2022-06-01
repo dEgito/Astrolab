541954ce2c8563c08ae5b7eba0f449669acf674a
Danielly Egito <egitodany@gmail.com>
2022-06-01 17:45:53 -0300

    feat(Home): adiciona estrutura da tela

---
+++ src/components/PrimaryButton/index.js
@@ -0,0 +1,9 @@
+import Button from "./style";
+
+function Primary ({children}){
+    return (
+        <Button>{children}</Button>
+    )
+}
+
+export default Primary;
--- src/pages/Home/index.jsx
+++
@@ -1,5 +0,0 @@
-function Login() {
-  return <h1>Astrolab is borning</h1>;
-}
-
-export default Login;
