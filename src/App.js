89dbdc1929e62cac3ec0830266a007f99502e59e
Danielly Egito <egitodany@gmail.com>
2022-06-01 07:37:57 -0300

    feat: start project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Login from "./pages/Home";
+
+function App() {
+  return (
+    <Login />
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h1>Astrolab is borning</h1>;
+}
+
+export default Login;
