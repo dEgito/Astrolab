42d252ee852193beb0cfc4d3b91c5a23f6a5eee6
Danielly Egito <egitodany@gmail.com>
2022-06-01 20:42:45 -0300

    feat: adiciona footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import Primary from "../../components/PrimaryButton";
 import Secundary from "../../components/SecundaryButton";
 import Logo from "../../assets/logo.png";
 import Planet from "../../assets/planet-icon.png";
+import Coffee from "../../assets/Coffee.svg";
 
 function Home() {
   return (
@@ -18,6 +19,13 @@ function Home() {
         <Primary>Login</Primary>
         <Secundary>Cadastro</Secundary>
       </Buttons>
+
+      <footer>
+        <span>
+          Create with <img src={Coffee} /> by
+          <a href="https://github.com/dEgito" target="_blank">@dEgito</a>
+        </span>
+      </footer>
     </Container>
   );
 }
