36e0006eb66dd20dd3c760790a6e8db511ac995d
Danielly Egito <egitodany@gmail.com>
2022-06-01 19:24:32 -0300

    refactor (Home): configura animação do ícone

--- src/components/PrimaryButton/style.js
+++ src/components/PrimaryButton/style.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: Inter;
   font-size: 0.8rem;
   font-weight: 600;
   color: #000000;
@@ -11,5 +16,10 @@ const Button = styled.button`
   border-radius: 30px;
   transition: all 0.3s ease-out;
   background-color: #FFC700;
+
+  &:hover {
+    font-size: .9rem;
+    font-weight: 700;
+  }
 `;
 export default Button;
--- src/components/SecundaryButton/style.js
+++ src/components/SecundaryButton/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  font-family: Inter;
   font-size: 0.8rem;
   font-weight: 600;
   min-width: 180px;
@@ -10,6 +11,11 @@ const Button = styled.button`
   color: #ffffff;
   border-bottom: 4px solid #ffffff;
   background: none;
+
+  &:hover {
+    font-size: .9rem;
+    font-weight: 700;
+  }
 `;
 
 export default Button;
