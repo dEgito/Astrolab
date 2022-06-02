ba09bd9042128b09c2e022d58aa34dff57aa38ff
Danielly Egito <egitodany@gmail.com>
2022-06-02 15:32:00 -0300

    refactor (Login): ajusta radious do header

--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -1,17 +1,29 @@
 import Background from "../../assets/bg.png";
 import styled from "styled-components";
 
+export const Container = styled.body`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background-color: #560526;
+`;
+
 export const Contents = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 1rem;
-  height: 85vh;
+  width: 100%;
+  height: 100%;
   color: #ffffff;
   font-family: Inter;
   background: url(${Background}) repeat;
   background-color: #000;
+  border-radius: 30px 30px 0 0;
 
   div {
     display: flex;
