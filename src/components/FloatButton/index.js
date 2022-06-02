0d058627443a71467cd3ace60e5c53e131a4887a
Danielly Egito <egitodany@gmail.com>
2022-06-02 16:24:54 -0300

    feat: adiciona botão flutuante

---
+++ src/components/FloatButton/index.js
@@ -0,0 +1,14 @@
+import { Button } from "./style";
+import Github from "../../assets/github-icon.svg";
+
+function FloatButton({ children }) {
+  return (
+    <a href="https://github.com/dEgito" target="_blank">
+      <Button>
+        <img src={Github} alt="icone-github"/>
+      </Button>
+    </a>
+  );
+}
+
+export default FloatButton;
