5a9a8c8dc69b14e6ca65eabe3a80e5f545eaa4ed
Danielly Egito <egitodany@gmail.com>
2022-06-03 12:35:44 -0300

    refactor: finaliza estilização do 'Contents'

--- src/pages/Token/index.js
+++ src/pages/Token/index.js
@@ -1,26 +1,28 @@
 import Input from "../../components/Input";
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/logo.png";
 import { Container, Contents, Header } from "./style";
-
 
 function Token() {
   return (
     <Container>
-        <header>
-            <h2>Verificação</h2>
-            <p>Enviamos o código para o seu e-mail</p>
-        </header>
+      <header>
+        <h2>Verificação</h2>
+        <p>Enviamos o código para o seu e-mail</p>
+      </header>
 
-        <Contents>
-            <p>Digite o código de ativação:</p>
-            <Input placeholder="000 000"/>
-            <a>Não recebeu? <span>Reenviar</span></a>
+      <Contents>
+        <p>Digite o código de ativação:</p>
+        <Input placeholder="000 000" />
+        <a>
+          Não recebeu? <span>Reenviar</span>
+        </a>
 
-            <footer>
-                <img src={Logo} alt="logo-astrolab"/>
-            </footer>
-        </Contents>
-        
+        <footer>
+          <a href="https://github.com/dEgito" target="_blank">
+            <img src={Logo} alt="logo-astrolab" />
+          </a>
+        </footer>
+      </Contents>
     </Container>
   );
 }
