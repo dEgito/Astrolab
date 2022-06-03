f33409788e89b6496394b2d3ee9fbde36bab9ea2
Danielly Egito <egitodany@gmail.com>
2022-06-03 14:10:45 -0300

    refactor: reposiciona o header

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,27 +1,33 @@
 import Footer from "../../components/Footer";
 import Primary from "../../components/PrimaryButton";
+import Header from "../../components/Header";
 
 import Planet from "../../assets/polygon-planet.png";
-import { Container, Image } from "./style";
+import FloatButton from "../../components/FloatButton"
+import { Container, Contents, Image } from "./style";
 import Icon from "./components/PlanetIcon";
 
 function Welcome() {
   return (
     <>
       <Container>
-        <div>
-          <h3>SEU ACESSO FOI LIBERADO</h3>
-          <p>
-            Seja muito bem-vinde,
-            <br />
-            <span>Tripulante</span>!
-          </p>
-        </div>
+        <Header />
 
-        {/* <Image src={Planet} /> */}
-        <Icon />
-        <Primary>Jogar</Primary>
-        <Footer />
+        <Contents>
+          <div>
+            <h3>SEU ACESSO FOI LIBERADO</h3>
+            <p>
+              Seja muito bem-vinde,
+              <br />
+              <span>Tripulante</span>!
+            </p>
+          </div>
+
+          {/* <Image src={Planet} /> */}
+          <Icon />
+          <Primary>Jogar</Primary>
+          <FloatButton />
+        </Contents>
       </Container>
     </>
   );
--- src/pages/Welcome/style.js
+++ src/pages/Welcome/style.js
@@ -5,36 +5,44 @@ export const Container = styled.body`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  gap: 1rem;
-
+  height: 100vh;
   margin: 0;
   padding: 0;
-  height: 100vh;
+  background-color: #560526;
+`;
 
+export const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 4rem 0;
+  gap: 1rem;
+  width: 100%;
+  height: 100%;
   color: #ffffff;
   font-family: Inter;
   background: url(${Background}) repeat;
   background-color: #000;
+  border-radius: 30px 30px 0 0;
 
   div {
     display: flex;
     flex-direction: column;
-    align-items: center;
     text-align: center;
+    gap: 0.5rem;
   }
 
   p {
-      margin: 0;
+    margin: 0;
   }
 
   span {
-        text-transform: uppercase;
-        color: #ffc700;
-        line-height: 2.5rem;
-        font-size: 1.2rem;
-        font-weight: 600;
-    }
+    text-transform: uppercase;
+    color: #ffc700;
+    line-height: 2.5rem;
+    font-size: 1.2rem;
+    font-weight: 600;
+  }
 `;
 
 export const Image = styled.img`
