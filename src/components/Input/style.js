bb283e01286af29d6e83a860ccb19b7e3f66b703
Danielly Egito <egitodany@gmail.com>
2022-06-03 12:46:24 -0300

    feat: adiciona pros nos inputs

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,16 +1,52 @@
 import styled from "styled-components";
 
 export const TextInput = styled.input`
-  font-size: 0.8rem;
-  width: 16rem;
   height: 2rem;
   border: none;
   border-radius: 4px;
   padding: 0.3rem 1rem;
   outline: none;
 
+  width: ${(props) => {
+    switch (props.type) {
+      case "token":
+        return "10rem";
+
+      default:
+        return "16rem;";
+    }
+  }};
+
+  font-size: ${(props) => {
+    switch (props.type) {
+      case "token":
+        return "1rem;";
+
+      default:
+        return "0.8rem;";
+    }
+  }};
+
+  text-align: ${(props) => {
+    switch (props.type) {
+      case "token":
+        return "center;";
+
+      default:
+        return "left;";
+    }
+  }};
+
   &:focus {
-    font-size: 0.9rem;
     outline: 2px solid #ffc700;
+    font-size: ${(props) => {
+      switch (props.type) {
+        case 'token':
+          return '1.1rem'
+
+        default:
+          return '0.9rem'
+      }
+    }};
   }
 `;
