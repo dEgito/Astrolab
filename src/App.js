359c1dc0c603ded0d66560ff7d07ac65503faf57
Danielly Egito <egitodany@gmail.com>
2022-06-06 16:20:15 -0300

    refactor: importa tela "welcome"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login"
 import Register from "./pages/Register";
 import Token from "./pages/Token";
+import Welcome from "./pages/Welcome"
 
 function App() {
   return (
