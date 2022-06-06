d5c7404a5626c4750b526795344cc570bc83c14c
Danielly Egito <egitodany@gmail.com>
2022-06-06 15:56:58 -0300

    feat: adiciona lógica de verificação dos inputs

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -6,7 +6,28 @@ import FloatButton from "../../components/FloatButton";
 import Google from "../../assets/google-icon.png"
 import { Container, Contents, Buttons } from "./style";
 
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
 function Register() {
+
+  const schema = yup.object().shape({
+    user: yup.string().max(40, "Seu nome não pode ser tão grande!").required('Campo obrigatório'),
+    email: yup.string().email('Insira um e-mail valido!').required('Campo obrigatório'),
+    password: yup.string().required('Campo obrigatório.'),
+    passwordConfirmation: yup.string().required('Campo obrigatório')
+    .oneOf([yup.ref('password')], 'Senhas não correspondentes!'),
+  })
+
+  const { register, handleSubmit, formState: { errors }, } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const validation = (dados) => {
+    dados !== undefined && onSubmit("login", dados)
+  }
+  
   return (
     <Container>
       <Header />
@@ -19,15 +40,19 @@ function Register() {
         </Primary>
         <p>ou</p>
 
-        <div>
+        <div onSubmit={handleSubmit(validation)}>
           <Input placeholder="Usuário" />
+          <span>{errors.user?.message}</span>
           <Input placeholder="E-mail" />
+          <span>{errors.email?.message}</span>
           <Input placeholder="Senha" />
+          <span>{errors.password?.message}</span>
           <Input placeholder="Confirmação de senha" />
+          <span>{errors.passwordConfirmation?.message}</span>
         </div>
 
         <Buttons>
-          <Primary>Cadastrar</Primary>
+          <Primary type="submit">Cadastrar</Primary>
           <Secundary>Já tenho conta</Secundary>
         </Buttons>
 
