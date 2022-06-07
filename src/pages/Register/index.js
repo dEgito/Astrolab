0d2d5a1aa25ecf9f79ddfe030ad1b9c8569761a5
Danielly Egito <egitodany@gmail.com>
2022-06-07 05:52:18 -0300

    undo: remove lógica de verificação dos modais

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -6,28 +6,7 @@ import FloatButton from "../../components/FloatButton";
 import Google from "../../assets/images/google-icon.png"
 import { Container, Contents, Buttons } from "./style";
 
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
-
 function Register() {
-
-  const schema = yup.object().shape({
-    user: yup.string().max(40, "Seu nome não pode ser tão grande!").required('Campo obrigatório'),
-    email: yup.string().email('Insira um e-mail valido!').required('Campo obrigatório'),
-    password: yup.string().required('Campo obrigatório.'),
-    passwordConfirmation: yup.string().required('Campo obrigatório')
-    .oneOf([yup.ref('password')], 'Senhas não correspondentes!'),
-  })
-
-  const { register, handleSubmit, formState: { errors }, } = useForm({
-    resolver: yupResolver(schema),
-  });
-
-  const validation = (dados) => {
-    dados !== undefined && onSubmit("login", dados)
-  }
-  
   return (
     <Container>
       <Header />
@@ -40,15 +19,11 @@ function Register() {
         </Primary>
         <p>ou</p>
 
-        <div onSubmit={handleSubmit(validation)}>
+        <div>
           <Input placeholder="Usuário" />
-          <span>{errors.user?.message}</span>
           <Input placeholder="E-mail" />
-          <span>{errors.email?.message}</span>
           <Input placeholder="Senha" />
-          <span>{errors.password?.message}</span>
           <Input placeholder="Confirmação de senha" />
-          <span>{errors.passwordConfirmation?.message}</span>
         </div>
 
         <Buttons>
